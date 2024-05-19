5e8987d7264fb01c724352349d510bc364b0cbf8
Kelly Yong <kelly.a.yong@gmail.com>
2024-05-19 05:29:56 -0400

    Clarify 3 ways to display response

--- example_program.py
+++ example_program.py
@@ -15,21 +15,22 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5000")
 
 
-# Set the personality type requesting for info as the request manually
+# Set the personality type requesting for info on as the request
 request = 'type1'
 
 # Send request and wait for response
 print(f"Searching for personality {request} ...")
 socket.send_string(request)
 
-# Get reply and print in CLI
+# Get response from microservice server
 message = socket.recv_string()
+
+# Print response in CLI
 print(f"Your Personality type is: {request}\n [ {message} ]")
 
 # Or write response to a text file
 with open('personality-result.txt', 'w') as file:
     file.write(message)
 
-
 # Or display as pop up message box
-pymsgbox.alert(message, request)
+pymsgbox.alert(message, request, button='OK')
